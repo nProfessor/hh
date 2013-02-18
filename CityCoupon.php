cbbfa5c3e8a0d8bfde1a5a388bffd7ee488db40c
nProfessor <n-professor@ya.ru>
2013-02-18 03:25:56 -0800

    Create CityCoupon.php

---
+++ CityCoupon.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: professor
+ * Date: 24.10.12
+ * Time: 19:00
+ * To change this template use File | Settings | File Templates.
+ */
+class CityCoupon extends Kupon
+{
+ public $xml="http://sitycoupon.ru/ex.php";
+ public $tags="citycoupon";
+}
